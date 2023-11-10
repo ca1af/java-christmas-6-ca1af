f5abfb21e7fddec6425d5be9832e7417d371ac4a
DK <laancer4@gmail.com>
2023-11-10 12:54:32 +0900

    refactor : 원시값을 포장

--- src/main/java/date/OrderDay.java
+++ src/main/java/date/OrderDay.java
@@ -1,15 +1,32 @@
 package date;
 
+import exeptions.ExceptionMessage;
+
 import java.util.Calendar;
 
 public class OrderDay {
+    private static final int START_DAY = 1;
+    private static final int END_DAY = 31;
+    private static final int THIS_YEAR = 2023;
+    private static final int D_DAY_EVENT_END_DAY = 25;
+    private static final int D_DAY_DISCOUNT_START_PRICE = 1000;
+    private static final int D_DAY_DISCOUNT_AMOUNT_PER_DAY = 100;
+    private static final int NOT_APPLICABLE = 0;
     private final Integer day;
     public OrderDay(int day) {
+        validateDay(day);
         this.day = day;
     }
+
+    private void validateDay(int day){
+        if (day < START_DAY || day > END_DAY){
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_DAY.getMessage());
+        }
+    }
+
     public int getDayOfWeek() {
         Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.YEAR, 2023);
+        calendar.set(Calendar.YEAR, THIS_YEAR);
         calendar.set(Calendar.MONTH, Calendar.DECEMBER);
         calendar.set(Calendar.DAY_OF_MONTH, this.day);
         return calendar.get(Calendar.DAY_OF_WEEK);
@@ -17,20 +34,19 @@ public class OrderDay {
 
     public boolean isWeekend() {
         int dayOfWeek = this.getDayOfWeek();
-        return dayOfWeek == 6 || dayOfWeek == 7;
+        return dayOfWeek == Calendar.FRIDAY || dayOfWeek == Calendar.SATURDAY;
     }
 
     public boolean isStarDay(){
         int dayOfWeek = this.getDayOfWeek();
-        return dayOfWeek == 1 || this.day == 25;
+        return dayOfWeek == START_DAY || this.day == D_DAY_EVENT_END_DAY;
     }
 
     public int getDiscountAmount() {
-        int startPrice = 1000;
-        int increasingDiscountAmount = (day - 1) * 100;
-        if (day > 25){
-            return 0;
+        int increasingDiscountAmount = (this.day - START_DAY) * D_DAY_DISCOUNT_AMOUNT_PER_DAY;
+        if (this.day > D_DAY_EVENT_END_DAY){
+            return NOT_APPLICABLE;
         }
-        return startPrice + increasingDiscountAmount;
+        return D_DAY_DISCOUNT_START_PRICE + increasingDiscountAmount;
     }
 }
