5d59e2101b11cc13a316d46fb6ad70064cdfb3fa
DK <laancer4@gmail.com>
2023-11-13 22:03:09 +0900

    fix : 쓸모없는 필드(name) 및 게터 제거

--- src/main/java/gift/FreeGift.java
+++ src/main/java/gift/FreeGift.java
@@ -5,14 +5,9 @@ import menu.category.Beverage;
 
 public enum FreeGift {
     FOR_CHRISTMAS_EVENT(Beverage.CHAMPAGNE);
-    private final String name;
     private final int price;
     FreeGift(Menu menu) {
-        this.name = menu.getName();
         this.price = menu.getPrice();
-    }
-    public String getName() {
-        return name;
     }
 
     public int getPrice() {
