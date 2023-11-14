7101e8aff69d2ec8839fad098975b14cb2b53d14
DK <laancer4@gmail.com>
2023-11-14 23:42:13 +0900

    feat : 구현 완료

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -6,7 +6,6 @@ import console.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
         DiscountDetailOutputView discountDetailOutputView = new DiscountDetailOutputView();
